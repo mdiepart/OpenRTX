27cece32d044f02a691d7c1a10bee11afe5dd5c7
Morgan Diepart <morgandiepart@gmail.com>
2024-07-13 00:29:42 +0200

    Updated NXP MK22 CMSIS pack

--- platform/mcu/CMSIS/Device/NXP/MK22FN512xxx12/Source/system_MK22F51212.c
+++ platform/mcu/CMSIS/Device/NXP/MK22FN512xxx12/Source/system_MK22F51212.c
@@ -79,7 +79,7 @@
  */
 
 #include <stdint.h>
-#include "../Include/MK22F51212.h"
+#include "fsl_device_registers.h"
 
 
 
@@ -89,35 +89,30 @@
 
 uint32_t SystemCoreClock = DEFAULT_SYSTEM_CLOCK;
 
-/* Interrupt vectors */
-extern uint32_t __Vectors[];
-
 /* ----------------------------------------------------------------------------
    -- SystemInit()
    ---------------------------------------------------------------------------- */
 
 void SystemInit (void) {
+#if ((__FPU_PRESENT == 1) && (__FPU_USED == 1))
+  SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2));    /* set CP10, CP11 Full Access */
+#endif /* ((__FPU_PRESENT == 1) && (__FPU_USED == 1)) */
 
-    SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2));
+#if (DISABLE_WDOG)
+  /* WDOG->UNLOCK: WDOGUNLOCK=0xC520 */
+  WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xC520); /* Key 1 */
+  /* WDOG->UNLOCK: WDOGUNLOCK=0xD928 */
+  WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xD928); /* Key 2 */
+  /* WDOG->STCTRLH: ?=0,DISTESTWDOG=0,BYTESEL=0,TESTSEL=0,TESTWDOG=0,?=0,?=1,WAITEN=1,STOPEN=1,DBGEN=0,ALLOWUPDATE=1,WINEN=0,IRQRSTEN=0,CLKSRC=1,WDOGEN=0 */
+  WDOG->STCTRLH = WDOG_STCTRLH_BYTESEL(0x00) |
+                 WDOG_STCTRLH_WAITEN_MASK |
+                 WDOG_STCTRLH_STOPEN_MASK |
+                 WDOG_STCTRLH_ALLOWUPDATE_MASK |
+                 WDOG_STCTRLH_CLKSRC_MASK |
+                 0x0100U;
+#endif /* (DISABLE_WDOG) */
 
-    #if (DISABLE_WDOG)
-    /* WDOG->UNLOCK: WDOGUNLOCK=0xC520 */
-    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xC520); /* Key 1 */
-    /* WDOG->UNLOCK: WDOGUNLOCK=0xD928 */
-    WDOG->UNLOCK = WDOG_UNLOCK_WDOGUNLOCK(0xD928); /* Key 2 */
-    /* WDOG->STCTRLH: ?=0,DISTESTWDOG=0,BYTESEL=0,TESTSEL=0,TESTWDOG=0,?=0,?=1,WAITEN=1,STOPEN=1,DBGEN=0,ALLOWUPDATE=1,WINEN=0,IRQRSTEN=0,CLKSRC=1,WDOGEN=0 */
-    WDOG->STCTRLH = WDOG_STCTRLH_BYTESEL(0x00) |
-                    WDOG_STCTRLH_WAITEN_MASK |
-                    WDOG_STCTRLH_STOPEN_MASK |
-                    WDOG_STCTRLH_ALLOWUPDATE_MASK |
-                    WDOG_STCTRLH_CLKSRC_MASK |
-                    0x0100U;
-    #endif
-
-    SystemInitHook();
-
-    /* Configure the Vector Table location add offset address ------------------*/
-    SCB->VTOR = (uint32_t)(&__Vectors[0]);
+  SystemInitHook();
 }
 
 /* ----------------------------------------------------------------------------
