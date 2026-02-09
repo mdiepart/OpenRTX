1f9dc87330486ef53742407c49857ddc78b83e52
Morgan Diepart <morgandiepart@gmail.com>
2026-02-09 21:57:49 +0100

    [WIP] Flash STM32 stub

--- platform/drivers/NVM/flash_stm32.c
+++ platform/drivers/NVM/flash_stm32.c
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: Copyright 2020-2026 OpenRTX Contributors
- * 
+ *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -9,6 +9,17 @@
 #include <stdio.h>
 #include <errno.h>
 #include "flash_stm32.h"
+
+int STM32Flash_init(struct nvmSTM32FlashDevice *dev)
+{
+
+}
+
+int STM32Flash_terminate(struct nvmSTM32FlashDevice *dev)
+{
+    (void) dev;
+    return 0;
+}
 
 static int nvm_api_read(const struct nvmDevice *dev, uint32_t offset,
                         void *data, size_t len)
@@ -44,7 +55,7 @@ static int nvm_api_erase(const struct nvmDevice *dev, uint32_t offset, size_t le
     return -ENOSYS; // Not implemented yet
 }
 
-const struct nvmOps stm32_flash_ops = 
+const struct nvmOps stm32_flash_ops =
 {
     .read = nvm_api_read,
     .write = nvm_api_write,
@@ -52,3 +63,10 @@ const struct nvmOps stm32_flash_ops =
     .sync = NULL,
 };
 
+const struct nvmInfo stm32_flash_info =
+{
+    .write_size = 1,
+    .erase_size = 4096,
+    .erase_cycles = 10000,
+    .device_info = NVM_FLASH | NVM_WRITE | NVM_ERASE;
+}
--- platform/drivers/NVM/flash_stm32.h
+++ platform/drivers/NVM/flash_stm32.h
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: Copyright 2020-2026 OpenRTX Contributors
- * 
+ *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -12,4 +12,32 @@
 /**
  * Device driver for STM32 internal-flash based storage.
  */
+struct nvmSTM32FlashDevice
+{
+    const void           *priv;
+    const struct nvmOps  *ops;
+    const struct nvmInfo *info;
+    int                  device;
+}
+
+/**
+ *Driver API functions and info.
+ */
+extern const struct nvmOps stm32_flash_ops;
+extern const struct nvmInfo stm32_flash_info;
+
+#define STM32_FLASH_DEVICE_DEFINE(name)     \
+static struct nvmSTM32FlashDevice name =    \
+{                                           \
+    .ops    = &stm32_flash_ops,             \
+    .info   = &stm32_flash_info,            \
+    .device = 0                             \
+};                                          \
+
+/**
+ *
+ */
+int STM32Flash_init(struct nvmSTM32FlashDevice *dev);
+
+int STM32Flash_terminate(struct nvmSTM32FlashDevice *dev);
 #endif
