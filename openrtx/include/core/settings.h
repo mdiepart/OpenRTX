5acd69fbd37b9776d44162b08a01120103984f8f
Morgan Diepart <morgandiepart@ikmail.com>
2026-02-22 21:35:30 +0100

    Implemented API to store device settings

--- openrtx/include/core/settings.h
+++ openrtx/include/core/settings.h
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: Copyright 2020-2026 OpenRTX Contributors
- * 
+ *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -9,6 +9,20 @@
 
 #include "hwconfig.h"
 #include <stdbool.h>
+
+/** \file
+ * The settings.h file is the header defining the structure for the device
+ * settings structure and the functions to load/save the settings to
+ * non-volatile memory.
+ * The settings are saved across two partitions using an A/B partitions scheme.
+ * The settings are thus saved first in partition A then partition B and so on.
+ * Each new settings structure is appended to the last one in the partition.
+ * When the partition is full, it is erased. Because of the A/B partition
+ * scheme, this ensures that there is always a copy of the settings existing
+ * on the device. If an error occurs during the save, or if the latest copy
+ * gets corruptes, there is at lease one previous copy stored in the other
+ * partition.
+ */
 
 typedef enum
 {
@@ -33,23 +47,23 @@ display_timer_t;
 
 typedef struct
 {
-    uint8_t brightness;           // Display brightness
-    uint8_t contrast;             // Display contrast
-    uint8_t sqlLevel;             // Squelch level
-    uint8_t voxLevel;             // Vox level
-    int8_t  utc_timezone;         // Timezone, in units of half hours
-    bool    gps_enabled;          // GPS active
-    char    callsign[10];         // Plaintext callsign
-    uint8_t display_timer   : 4,  // Standby timer
-            m17_can         : 4;  // M17 CAN
-    uint8_t vpLevel         : 3,  // Voice prompt level
-            vpPhoneticSpell : 1,  // Phonetic spell enabled
-            macroMenuLatch  : 1,  // Automatic latch of macro menu
+    uint8_t brightness;           ///< Display brightness
+    uint8_t contrast;             ///< Display contrast
+    uint8_t sqlLevel;             ///< Squelch level
+    uint8_t voxLevel;             ///< Vox level
+    int8_t  utc_timezone;         ///< Timezone, in units of half hours
+    bool    gps_enabled;          ///< GPS active
+    char    callsign[10];         ///< Plaintext callsign
+    uint8_t display_timer   : 4,  ///< Standby timer
+            m17_can         : 4;  ///< M17 CAN
+    uint8_t vpLevel         : 3,  ///< Voice prompt level
+            vpPhoneticSpell : 1,  ///< Phonetic spell enabled
+            macroMenuLatch  : 1,  ///< Automatic latch of macro menu
             _reserved       : 3;
-    bool    m17_can_rx;           // Check M17 CAN on RX
-    char    m17_dest[10];         // M17 destination
-    bool    showBatteryIcon;      // Battery display true: icon, false: percentage
-    bool    gpsSetTime;           // Use GPS to ajust RTC time
+    bool    m17_can_rx;           ///< Check M17 CAN on RX
+    char    m17_dest[10];         ///< M17 destination
+    bool    showBatteryIcon;      ///< Battery display true: icon, false: percentage
+    bool    gpsSetTime;           ///< Use GPS to ajust RTC time
 }
 __attribute__((packed)) settings_t;
 
@@ -78,5 +92,41 @@ static const settings_t default_settings =
     false,                        // Display battery icon
     false,                        // Update RTC with GPS
 };
+
+struct settings_storage_s;
+typedef struct settings_storage_s settings_storage_t;
+
+/**
+ * Initialize a settings_storage_t structure to save and load device settings.
+ *
+ * @param s pointer to a pre-allocated settings_storage_t structure to be initialized
+ * @param nvm_dev NVM device number in which to store the device settings
+ * @param part_A NVM device partition number for storage partition A
+ * @param part_B NVM device partition number for storage partition B
+ * @return 0 if successful, negative error code otherwise
+ */
+int settings_storage_init(settings_storage_t *s, const int nvm_dev,
+                          const int part_A, const int part_B);
+
+/**
+ * Load device settings from non-volatile memory.
+ *
+ * @param s pointer to an initialized settings_storage_t structure
+ * @param settings pointer to a settings_t structure where to write the loaded settings
+ * @return 0 if successful, negative error code otherwise
+ */
+int settings_storage_load(settings_storage_t *s, settings_t *settings);
+
+/**
+ * Save device settings to non-volatile memory. Will not perform any actual write
+ * if the settings haven't changed.
+ *
+ * @param s pointer to an initialized settings_storage_t structure
+ * @param settings pointer to a settings_t structure containing the settings to save
+ * @return 0 if successful, negative error code otherwise
+ */
+int settings_storage_save(settings_storage_t *s, const settings_t *settings);
+
+
 
 #endif /* SETTINGS_H */
