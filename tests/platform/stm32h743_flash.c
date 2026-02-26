e4dac03ff84b895ecaaeb5e7b76ae10f23fb6dc2
Morgan Diepart <morgandiepart@ikmail.com>
2026-02-26 22:15:39 +0100

    fixup! Added STM32Flash NVM tests for stm32h743

--- tests/platform/stm32h743_flash.c
+++ tests/platform/stm32h743_flash.c
@@ -51,105 +51,149 @@ int main(void)
 
     const size_t nb_tests = 15;
     uint8_t results[nb_tests];
+    size_t current_test = 0;
 
-    results[0] = (STM32Flash_init(&flash_driver_128k) == 0);
+    results[current_test++] = (STM32Flash_init(&flash_driver_128k) == 0);
 
     // Erase the full allocated area
     const uint32_t sector11 =
         0x8100000 + 2 * 0x20000; // 3rd sector of the OpenRTX area, 11th sector
     const size_t area_size = 0x40000; // 2 sectors of 128K
 
-    results[1] = (nvm_devErase(&flash_driver_128k, sector11, area_size) == 0);
+    results[current_test++] =
+        (nvm_devErase(&flash_driver_128k, sector11, area_size) == 0);
 
     // Fill the full area with consecutive numbers (32 bits numbers, 32 bytes access)
-    results[2] = true;
-    for (size_t i = 0; i < area_size / 4; i += 8) {
+    results[current_test] = true;
+    for (size_t i = 0; i < area_size / 8; i += 8) {
         uint32_t data[8] = {
             i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7
         };
         int ret = nvm_devWrite(&flash_driver_128k, sector11 + (i * 4),
                                (void *)(data), sizeof(data));
         if (ret < 0) {
-            printf("Test 3 failed: error %d while writing to flash.\r\n", ret);
-            results[2] = false;
+            printf("Test %d failed: error %d while writing to flash.\r\n",
+                   current_test + 1, ret);
+            results[current_test] = false;
+            break;
         }
     }
+    current_test++;
+
+    // Fills the area by requesting writes of 128 bytes
+    results[current_test] = true;
+    for (size_t i = area_size / 8; i < area_size / 4; i += 32) {
+        uint32_t data[8 * 4] = { i,      i + 1,  i + 2,  i + 3,  i + 4,  i + 5,
+                                 i + 6,  i + 7,  i + 8,  i + 9,  i + 10, i + 11,
+                                 i + 12, i + 13, i + 14, i + 15, i + 16, i + 17,
+                                 i + 18, i + 19, i + 20, i + 21, i + 22, i + 23,
+                                 i + 24, i + 25, i + 26, i + 27, i + 28, i + 29,
+                                 i + 30, i + 31 };
+        int ret = nvm_devWrite(&flash_driver_128k, sector11 + (i * 4),
+                               (void *)(data), sizeof(data));
+        if (ret < 0) {
+            printf("Test %d failed: error %d while writing to flash.\r\n",
+                   current_test + 1, ret);
+            results[current_test] = false;
+            break;
+        }
+    }
+    current_test++;
 
     uint32_t buffer[8] = { 0 };
 
     // These tests should fail
     // Write less than 32 bytes
-    results[4] =
+    results[current_test++] =
         (nvm_devWrite(&flash_driver_128k, sector11, &buffer, sizeof(buffer) / 2)
          == -EINVAL);
 
     // Misaligned write
-    results[5] =
+    results[current_test++] =
         (nvm_devWrite(&flash_driver_128k, sector11 + 4, &buffer, sizeof(buffer))
          == -EINVAL);
 
     // Erase less than a page
-    results[6] = (nvm_devErase(&flash_driver_128k, sector11, 16384)
-                  == -EINVAL); // Erase 16K
+    results[current_test++] = (nvm_devErase(&flash_driver_128k, sector11, 16384)
+                               == -EINVAL); // Erase 16K
 
     // Erase misaligned address
-    results[7] = (nvm_devErase(&flash_driver_128k, sector11 + 1, 131072)
-                  == -EINVAL); // off-by-1
+    results[current_test++] =
+        (nvm_devErase(&flash_driver_128k, sector11 + 1, 131072)
+         == -EINVAL); // off-by-1
 
     // Go beyond device memory size
     uint32_t top_addr = FLASH_BASE + (mem_size << 10);
-    results[8] =
+    results[current_test++] =
         (nvm_devRead(&flash_driver_128k, top_addr - 2, &buffer, sizeof(buffer))
          == -EINVAL);
-    results[9] =
+    results[current_test++] =
         (nvm_devWrite(&flash_driver_128k, top_addr - 2, &buffer, sizeof(buffer))
          == -EINVAL);
-    results[10] =
+    results[current_test++] =
         (nvm_devErase(&flash_driver_128k, top_addr - 131072, 2 * 131072)
          == -EINVAL);
 
     // Check that we read consecutive numbers.
-    results[11] = true;
+    results[current_test] = true;
     for (size_t i = 0; i < area_size / 4; i++) {
         uint32_t number;
-        int ret = nvm_devRead(&flash_driver_128k, sector11 + i * 4, &number, 4);
+        int ret = nvm_devRead(&flash_driver_128k, sector11 + (i * 4), &number,
+                              4);
         if (ret < 0) {
-            results[11] = false;
-            printf("Test 12 failed at i=%d. Read returned %ld.\r\n", i, ret);
+            results[current_test] = false;
+            printf("Test %d failed at i=%d. Read returned %ld.\r\n",
+                   current_test + 1, i, ret);
+            gfx_print(pos_line, FONT_SIZE_6PT, TEXT_ALIGN_LEFT, color_white,
+                      "ret=%d", ret);
+            pos_line.y += 12;
             break;
         }
         if (number != i) {
-            results[11] = false;
-            printf("Test 12 failed at i=%d. Read-back %ld.\r\n", i, number);
+            results[current_test] = false;
+            printf("Test %d failed at i=%d. Read-back %ld.\r\n",
+                   current_test + 1, i, number);
+            gfx_print(pos_line, FONT_SIZE_6PT, TEXT_ALIGN_LEFT, color_white,
+                      "wrong %d!=%d", i, number);
+            pos_line.y += 12;
             break;
         }
     }
+    current_test++;
+    gfx_print(pos_line, FONT_SIZE_6PT, TEXT_ALIGN_LEFT, color_white,
+              "Post read SR2: 0x%08X", FLASH->SR2);
+    pos_line.y += 12;
 
     // Erase second sector of the area
-    results[12] = (nvm_devErase(&flash_driver_128k, sector11 + 0x20000, 0x20000)
-                   == 0);
+    results[current_test++] =
+        (nvm_devErase(&flash_driver_128k, sector11 + 0x20000, 0x20000) == 0);
 
-    // Check that we still read consecutive numbers.
-    results[13] = true;
+    // Check that we still read consecutive numbers in first sector
+    __DSB();
+    results[current_test] = true;
     for (size_t i = 0; i < 0x20000 / 4; i++) {
         uint32_t number;
         nvm_devRead(&flash_driver_128k, sector11 + (i * 4), &number, 4);
         if (number != i) {
-            results[13] = false;
-            printf("Test 14 failed at i=%d. Read-back %ld.\r\n", i, number);
+            results[current_test] = false;
+            printf("Test %d failed at i=%d. Read-back %ld.\r\n",
+                   current_test + 1, i, number);
         }
     }
+    current_test++;
 
     // Check that we read 0xFFFFFFFF.
-    results[14] = true;
+    results[current_test] = true;
     for (size_t i = 0x20000 / 4; i < 0x20000 / 2; i++) {
         uint32_t number;
         nvm_devRead(&flash_driver_128k, sector11 + (i * 4), &number, 4);
         if (number != 0Xffffffff) {
-            results[14] = false;
-            printf("Test 15 failed at i=%d. Read-back %ld.\r\n", i, number);
+            results[current_test] = false;
+            printf("Test %d failed at i=%d. Read-back %ld.\r\n",
+                   current_test + 1, i, number);
         }
     }
+    current_test++;
 
     int nb_failed = 0;
     point_t list_pt = pos_line;
